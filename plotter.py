d1ef6188c0990ab505357f25536c200befa1d238
jkania7 <jkania@alumni.cmu.edu>
2018-04-04 12:51:41 -0400

    fixed plots

--- plotter.py
+++ plotter.py
@@ -25,10 +25,17 @@ x2 =  np.arange(np.amin(np.log10(fluxes[2:6,0])), np.max(np.log10(fluxes[2:6,0])
 p1 = np.poly1d(line1)
 p2 = np.poly1d(line2)
 
+
 popt1, pcov1 = curve_fit(line, np.log10(fluxes[0:3,0]),np.log10(fluxes[0:3,1]))
 popt2, pcov2 = curve_fit(line, np.log10(fluxes[2:6,0]),np.log10(fluxes[2:6,1]))
 perr1 = np.sqrt(np.diag(pcov1))
 perr2 = np.sqrt(np.diag(pcov2))
+popt1line, pcov1line = curve_fit(line, fluxes[0:3,0],fluxes[0:3,1])
+popt2line, pcov2line = curve_fit(line, fluxes[2:6,0],fluxes[2:6,1])
+perr1line = np.sqrt(np.diag(pcov1))
+perr2line = np.sqrt(np.diag(pcov2))
+
+
 print("area under the curve = {0} uJy".format(s.integrate.simps(fluxes[:,1],fluxes[:,0])))
 #splineFull = CubicSpline(fluxes[:,0],fluxes[:,1])
 #xFull =  np.arange(np.amin(fluxes[:,0]), np.max(fluxes[:,0]),0.01)
@@ -44,7 +51,10 @@ print("area under the curve = {0} uJy".format(s.integrate.simps(fluxes[:,1],flux
 #print("Area under spline = {0)".format(splineFull.integrate()(22.1)-splineFull.integrate.(1.63)))
 fig1 = plt.figure()#plot fluxes
 ax1 = fig1.add_subplot(111)
-ax1.errorbar(np.log10(fluxes[:,0]),np.log10(fluxes[:,1]),yerr=np.multiply(np.divide(np.log10(fluxes[:,1]),fluxes[:,1]),fluxes[:,2]),fmt='o')
+
+print np.log10(np.add(fluxes[:,1], fluxes[:,2]))
+#ax1.errorbar(np.log10(fluxes[:,0]),np.log10(fluxes[:,1]),yerr=np.multiply(np.divide(np.log10(fluxes[:,1]),fluxes[:,1]),fluxes[:,2]),fmt='o')
+ax1.errorbar(np.log10(fluxes[:,0]),np.log10(fluxes[:,1]),yerr=[np.subtract(np.log10(np.subtract(fluxes[:,1],fluxes[:,2])),np.log10(fluxes[:,1])),-np.subtract(np.log10(np.add(fluxes[:,1],fluxes[:,2])),np.log10(fluxes[:,1]))],fmt='o')
 #print(xspline)
 #print(spline(xspline))
 ax1.plot(xspline,spline(xspline))
@@ -72,7 +82,7 @@ fig2.savefig('spectral index.png', dpi=300,  bbox_inches='tight')
 
 fig3 = plt.figure()#plot fluxes
 ax3 = fig3.add_subplot(111)
-ax3.errorbar(np.log10(fluxes[:,0]),np.log10(fluxes[:,1]),yerr=np.multiply(np.divide(np.log10(fluxes[:,1]),fluxes[:,1]),fluxes[:,2]),fmt='o')
+ax3.errorbar(np.log10(fluxes[:,0]),np.log10(fluxes[:,1]),yerr=[np.subtract(np.log10(np.subtract(fluxes[:,1],fluxes[:,2])),np.log10(fluxes[:,1])),-np.subtract(np.log10(np.add(fluxes[:,1],fluxes[:,2])),np.log10(fluxes[:,1]))],fmt='o')
 #print(xspline)
 #print(spline(xspline))
 #ax3.plot(xspline,spline(xspline))
@@ -84,5 +94,7 @@ ax3.set_ylabel(r"$\log_{10}$(Flux Density [$\mu$Jy])")
 ax3.legend([r"Best Fit Line ({0:.3f}$\pm${1:.3f})x+{2:.3f}$\pm${3:.3f}".format(popt1[0],perr1[0],popt1[1],perr1[1]),"Best Fit Line ({0:.3f}$\pm${1:.3f})x+{2:.3f}$\pm${3:.3f}".format(popt2[0],perr2[0],popt2[1],perr2[1]),"Measured Flux Density"], loc='best')
 ax3.margins(0.05)
 fig3.savefig('fluxVsFreqLineFit.png', dpi=300, bbox_inches='tight')
+
+
 
 plt.show()
